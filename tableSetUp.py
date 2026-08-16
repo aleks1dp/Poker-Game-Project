26418f612484f73a69b3aa19c6a2de15af2a18ae
aleksandra1dp <150381037+aleksandra1dp@users.noreply.github.com>
2026-08-16 07:59:30 -0500

    Add set-up

---
+++ tableSetUp.py
@@ -0,0 +1,9 @@
+startingStack = 1000
+smallBlind = 5
+bigBlind = 10
+
+#Order of players in preflop and postflop
+preflop = ["UTG", "HG", "CO", "BTN", "SB", "BB"]
+postflop = ["SB", "BB", "UTG", "HG", "CO", "BTN"]
+
+betSize = 0.5 
